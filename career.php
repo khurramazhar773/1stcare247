07563e1ccd7d450e3fc6114e8a3f94485e4398f9
Ali-imran-developer <ai8440018@gmail.com>
2024-10-24 01:39:41 +0500

    Normalize line endings

--- career.php
+++ career.php
@@ -4,21 +4,19 @@
 <head>
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-      integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
    <link rel="stylesheet" href="assets/css/animate.css">
    <link rel="stylesheet" href="assets/bootstarp/bootstrap.min.css">
    <link rel="stylesheet" href="assets/css/super-classes.css">
    <link rel="stylesheet" href="assets/css/style.css">
    <link rel="stylesheet" href="assets/css/custom-style.css">
    <link rel="stylesheet" href="assets/css/mobile.css">
-
    <title>1stCare | Career</title>
+
 </head>
 
 <body>
    <!-- top-bar-section-->
-
    <?php
    // Attach header.php
    require 'header.php';
@@ -36,11 +34,10 @@
                <div class="w-100 float-left generic-banner-con text-xl-left text-lg-left text-center">
                   <div class="container">
                      <div class="generic-banner-content text-white text-center">
-                        <h1>Carrer</h1>
+                        <h1>Career</h1>
                         <p class="text-white mb-0">
-                           Providing personalized, expert care with compassion, <br /> ensuring your well-being at every
-                           step
-                           of your health journey.
+                        Join our team of dedicated healthcare professionals—explore<br>
+                         current opportunities and take the next step in your career today.
                         </p>
                      </div>
                   </div>
@@ -53,59 +50,56 @@
 
    <div class="container">
       <section class="hiring-section">
-         <h1 class="hiring-heading r">We Are Hiring</h1>
-         <p>1st care offers opportunities across the UK for experienced health and social care professionals who
-            prioritize a service user-centered approach in their practice.</p>
-         <p>If you have at least 6 months of experience in any of the following areas, we would love to hear from you:
-         </p>
+         <h1 class="hiring-heading">We Are Hiring</h1>
+         <p class="career-text">1st care offers opportunities across the UK for experienced health and social care professionals who prioritize a service user-centered approach in their practice.</p>
+         <p class="career-text">If you have at least 6 months of experience in any of the following areas, we would love to hear from you:</p>
+         <div class="bullet-points">
+            <ul class="job-list pt-4">
+               <li style="color:#52656D;">Elderly Care Assistants</li>
+               <li style="color:#52656D;">Registered Nurses (RGN & RMN) for hospitals and care facilities</li>
+               <li style="color:#52656D;">Support Specialists – Learning Disabilities & Mental Health</li>
+               <li style="color:#52656D;">Healthcare Assistants for hospitals and care homes</li>
+               <li style="color:#52656D;">Locum General Practitioners (GPs)</li>
+               <li style="color:#52656D;">Licensed Social Workers</li>
+            </ul>
 
-         <ul class="job-list pt-4">
-            <li>Elderly Care Assistants</li>
-            <li>Registered Nurses (RGN & RMN) for hospitals and care facilities</li>
-            <li>Support Specialists – Learning Disabilities & Mental Health</li>
-            <li>Healthcare Assistants for hospitals and care homes</li>
-            <li>Locum General Practitioners (GPs)</li>
-            <li>Licensed Social Workers</li>
-         </ul>
-
-
-         <p class="pt-5">If you have at least 6 months of relevant
+         </div>
+         <p class="pt-5 career-text">If you have at least 6 months of relevant
             experience, contact our dedicated recruitment team during
             office hours at +01 23 4567 8901 or +01 23 4567 8901.
             You can request an application pack by post or download the application form from the "Document Library"
             section on the 1st Med website.
             Please note that all job offers are contingent upon an enhanced DBS check.</p>
-         <p class="pt-2">For complete details, explore our current vacancies listed below.
+         <p class="pt-2 career-text">For complete details, explore our current vacancies listed below.
             To apply, simply fill out our Registration Form, upload your details, and we'll be in touch shortly.</p>
       </section>
    </div>
 
+
+
    <!-- header-and-banner-section -->
-   <section class=" blog-posts w-100 float-left">
+   <section class=" blog-posts w-100 float-left ">
       <div class="container">
          <div class="row">
             <div id="blog" class="col-xl-12">
                <div class="row">
-                  <div class="col-xl-6">
+                  <div class="col-xl-6 ">
                      <div class="float-left w-100 post-item border mb-4">
                         <div class="post-item-wrap position-relative">
                            <div class="post-image">
                               <a href="#">
-                                 <img alt=""
-                                    src="assets/image/care-service-banner1.png">
+                                 <img alt="" src="assets/image/care-service-banner1.png">
                               </a>
 
-                              <!--post-image-->
+
                            </div>
                            <div class="post-item-description">
-                              <h2>
-                                 <a href="#">Standard post with a single image </a>
+                              <h2 class="card-heading">
+                                 <a href="#">Elderly Care Assistants </a>
                               </h2>
 
-                              <p>Curabitur pulvinar euismod ante, ac sagittis ante posuere ac. Vivamus luctus commodo
-                                 dolor porta feugiat. Fusce at velit id ligula ac sagittis ante posuere ac pharetra
-                                 laoreet commodo dolor porta.</p>
-                              <div class="read-btn"><a href="#" class="read-more-btn">Read More </a></div>
+                              <p>Discover highly skilled Care Assistants ready to provide flexible, quality healthcare services when and where you need them. Our experienced team ensures continuity of care tailored to your practice's needs.</p>
+                              <div class="read-btn"><a href="contact.php" class="read-more-btn">Apply Now </a></div>
 
                               <!--post-item-description-->
                            </div>
@@ -120,21 +114,18 @@
                         <div class="post-item-wrap position-relative">
                            <div class="post-image">
                               <a href="#">
-                                 <img alt=""
-                                    src="assets/image/care-service-banner2.png">
+                                 <img alt="" src="assets/image/care-service-banner2.png">
                               </a>
 
-                              <!--post-image-->
+
                            </div>
                            <div class="post-item-description">
 
                               <h2>
-                                 <a href="#">Standard post with a single image </a>
+                                 <a href="#">Registered Nurses </a>
                               </h2>
-                              <p>Curabitur pulvinar euismod ante, ac sagittis ante posuere ac. Vivamus luctus commodo
-                                 dolor porta feugiat. Fusce at velit id ligula ac sagittis ante posuere ac pharetra
-                                 laoreet commodo dolor porta.</p>
-                              <div class="read-btn"><a href="#" class="read-more-btn">Read More </a></div>
+                              <p>Our experienced nurses deliver compassionate, patient-centered care across various specialties.Quality healthcare services when and where you need , reliable nursing staff to support your healthcare team.</p>
+                              <div class="read-btn"><a href="contact.php" class="read-more-btn">Apply Now</a></div>
                               <!--post-item-description-->
                            </div>
                            <!--post-item-wrap-->
@@ -156,12 +147,10 @@
                            <div class="post-item-description">
 
                               <h2>
-                                 <a href="#">Standard post with a single image </a>
+                                 <a href="#">Support Specialists</a>
                               </h2>
-                              <p>Curabitur pulvinar euismod ante, ac sagittis ante posuere ac. Vivamus luctus commodo
-                                 dolor porta feugiat. Fusce at velit id ligula ac sagittis ante posuere ac pharetra
-                                 laoreet commodo dolor porta.</p>
-                              <div class="read-btn"><a href="#" class="read-more-btn">Read More </a></div>
+                              <p>Our Support Specialists provides skilled, We ensure high-quality, compassionate care tailored to your needs. We offer flexible staffing solutions to ensure seamless support for your medical practice.</p>
+                              <div class="read-btn"><a href="contact.php" class="read-more-btn">Apply Now</a></div>
                               <!--post-item-description-->
                            </div>
                            <!--post-item-wrap-->
@@ -175,8 +164,7 @@
                         <div class="post-item-wrap position-relative">
                            <div class="post-image">
                               <a href="#">
-                                 <img alt=""
-                                    src="assets/image/care-service-banner4.png">
+                                 <img alt="" src="assets/image/care-service-banner4.png">
                               </a>
 
                               <!--post-image-->
@@ -184,12 +172,10 @@
                            <div class="post-item-description">
 
                               <h2>
-                                 <a href="#">Standard post with a single image </a>
+                                 <a href="#">Healthcare Assistants </a>
                               </h2>
-                              <p>Curabitur pulvinar euismod ante, ac sagittis ante posuere ac. Vivamus luctus commodo
-                                 dolor porta feugiat. Fusce at velit id ligula ac sagittis ante posuere ac pharetra
-                                 laoreet commodo dolor porta.</p>
-                              <div class="read-btn"><a href="#" class="read-more-btn">Read More </a></div>
+                              <p>Our Healthcare Assistants provide expert medication management and personalized care to enhance patient outcomes. We offer flexible staffing solutions to meet your pharmacy service needs</p>
+                              <div class="read-btn"><a href="contact.php" class="read-more-btn">Apply Now </a></div>
                               <!--post-item-description-->
                            </div>
                            <!--post-item-wrap-->
@@ -211,12 +197,10 @@
                            <div class="post-item-description">
 
                               <h2>
-                                 <a href="#">Standard post with a single image </a>
+                                 <a href="#">Locum General Practitioner </a>
                               </h2>
-                              <p>Curabitur pulvinar euismod ante, ac sagittis ante posuere ac. Vivamus luctus commodo
-                                 dolor porta feugiat. Fusce at velit id ligula ac sagittis ante posuere ac pharetra
-                                 laoreet commodo dolor porta.</p>
-                              <div class="read-btn"><a href="#" class="read-more-btn">Read More </a></div>
+                              <p>Our Locum GP specialists focus on personalized recovery plans to help patients regain strength and independence. We provide expert support across physical, occupational, and speech therapies.</p>
+                              <div class="read-btn"><a href="contact.php" class="read-more-btn">Apply Now </a></div>
                               <!--post-item-description-->
                            </div>
                            <!--post-item-wrap-->
@@ -238,12 +222,10 @@
                            <div class="post-item-description">
 
                               <h2>
-                                 <a href="#">Standard post with a single image </a>
+                                 <a href="#">Licensed Social Workers </a>
                               </h2>
-                              <p>Curabitur pulvinar euismod ante, ac sagittis ante posuere ac. Vivamus luctus commodo
-                                 dolor porta feugiat. Fusce at velit id ligula ac sagittis ante posuere ac pharetra
-                                 laoreet commodo dolor porta.</p>
-                              <div class="read-btn"><a href="#" class="read-more-btn">Read More </a></div>
+                              <p>Our social working team offers comprehensive care, addressing a wide range of medical conditions with expertise and compassion. We ensure high-quality, personalized treatment for patients of all ages.</p>
+                              <div class="read-btn"><a href="contact.php" class="read-more-btn">Apply Now </a></div>
                               <!--post-item-description-->
                            </div>
                            <!--post-item-wrap-->
@@ -253,56 +235,57 @@
                      <!--col-->
                   </div>
                </div>
+               <!--blog-->
             </div>
+
          </div>
+         <!--container-->
       </div>
    </section>
-
+   <!-- FOOTER -->
+   <!-- logo-section -->
    <div class="w-100 float-left logo-con">
       <div class="container">
          <div class="logo-inner-con">
             <div class="row">
                <div class="col-lg-2 col-md-2 col-sm-4 col-4">
                   <figure class="mb-0">
-                     <img src="assets/image/logo1.png" alt="client-logo-img" class="img-fluid">
-                  </figure>
-               </div>
-
-               <div class="col-lg-2 col-md-2 col-sm-4 col-4">
-                  <figure class="mb-0">
-                     <img src="assets/image/logo2.png" alt="client-logo-img" class="img-fluid">
+                     <img src="assets/image/qwerty1.png" alt="client-logo-img" class="img-fluid">
                   </figure>
                </div>
                <div class="col-lg-2 col-md-2 col-sm-4 col-4">
                   <figure class="mb-0">
-                     <img src="assets/image/logo3.png" alt="client-logo-img" class="img-fluid mb-0">
+                     <img src="assets/image/qwerty2.png" alt="client-logo-img" class="img-fluid">
                   </figure>
                </div>
                <div class="col-lg-2 col-md-2 col-sm-4 col-4">
                   <figure class="mb-0">
-                     <img src="assets/image/logo4.png" alt="client-logo-img" class="img-fluid mb-0">
+                     <img src="assets/image/qwerty3.png" alt="client-logo-img" class="img-fluid">
                   </figure>
                </div>
                <div class="col-lg-2 col-md-2 col-sm-4 col-4">
                   <figure class="mb-0">
-                     <img src="assets/image/logo5.png" alt="client-logo-img" class="img-fluid mb-0">
+                     <img src="assets/image/qwerty4.png" alt="client-logo-img" class="img-fluid mb-0">
                   </figure>
                </div>
                <div class="col-lg-2 col-md-2 col-sm-4 col-4">
                   <figure class="mb-0">
-                     <img src="assets/image/logo3.png" alt="client-logo-img" class="img-fluid mb-0">
+                     <img src="assets/image/qwerty5.png" alt="client-logo-img" class="img-fluid mb-0">
+                  </figure>
+               </div>
+               <div class="col-lg-2 col-md-2 col-sm-4 col-4">
+                  <figure class="mb-0">
+                     <img src="assets/image/qwerty2.png" alt="client-logo-img" class="img-fluid mb-0">
                   </figure>
                </div>
             </div>
          </div>
       </div>
    </div>
-
    <!-- logo-section -->
    <!-- weight-footer-section -->
    <?php require 'footer.php'; ?>
    <!-- footer-section -->
-
    <script src="assets/js/jquery-3.6.0.min.js"> </script>
    <script src="assets/js/popper.min.js"> </script>
    <script src="assets/js/wow.js"></script>
