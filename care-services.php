e6a41a24b6f290144730c8d2ea64e46ecf7ca6e2
khurramazhar773 <khurramazhar12346@gmail.com>
2024-10-24 15:01:16 +0500

    Updated

--- care-services.php
+++ care-services.php
@@ -46,203 +46,23 @@
    <?php require 'navbar.php'; ?>
 
     <!-- header-and-banner-section -->
-   <section class="blog-posts w-100 float-left mt-5">
-      <div class="container">
-         <div class="row">
-            <div id="blog" class="col-xl-12">
-               <div class="row">
-                  <!-- Example of a blog post -->
-                  <div class="col-xl-6">
-                     <div class="float-left w-100 post-item border mb-4">
-                        <div class="post-item-wrap position-relative">
-                           <div class="post-image">
-                              <div class="CareImage-banner">
-                                 <img alt="" src="assets/image/care-service-banner1.png">
-                              </div>
-                           </div>
-                           <div class="post-item-description blog-component">
-                              <h2 style="font-size: 23px;">
-                                 <a class="blog-links">Locum GP’s</a>
-                              </h2>
-                              <p class="blog-links">
-                              Discover highly skilled locum GPs ready to provide flexible, quality healthcare services when and where you need them. Our experienced team ensures continuity of care tailored to your practice's needs.
-                              </p>
-                              <!-- Initially hidden content -->
-                              <div class="extra-content hidden">
-                                 <p class="blog-links">
-                                    Locum GPs play a critical role in ensuring that healthcare practices continue
-                                    to provide services even when regular staff are unavailable.
-                                 </p>
-                                 <p class="blog-links">
-                                    By offering flexible solutions, locum GPs support various healthcare settings.
-                                 </p>
-                              </div>
-                              <a class="read-more item-link blog-links">Explore Our Care Services</a>
-                           </div>
+   <!-- banner-start -->
+   <section>
+                    <div class="w-100 float-left generic-banner-con text-xl-left text-lg-left text-center">
+                        <div class="container">
+                            <div class="generic-banner-content text-white text-center">
+                                <h1>Care Services</h1>
+                                <p class="text-white mb-0">
+                                    Providing personalized, expert care with compassion, <br /> ensuring your well-being
+                                    at every
+                                    step
+                                    of your health journey.
+                                </p>
+                            </div>
                         </div>
-                     </div>
-                  </div>
-                  <!-- Repeat similar blog structure for other columns... -->
-                  <div class="col-xl-6">
-                     <div class="float-left w-100 post-item border mb-4">
-                        <div class="post-item-wrap position-relative">
-                           <div class="post-image">
-                              <div class="CareImage-banner">
-                                 <img alt="" src="assets/image/care-service-banner2.png">
-                              </div>
-                           </div>
-                           <div class="post-item-description blog-component">
-                              <h2 style="font-size: 23px;">
-                                 <a class="blog-links">Nurses</a>
-                              </h2>
-                              <p class="blog-links">
-                                 Our pharmacists provide expert medication management and personalized care to enhance
-                                 patient outcomes. We offer flexible staffing solutions.
-                              </p>
-                              <!-- Initially hidden content -->
-                              <div class="extra-content hidden">
-                                 <p class="blog-links">
-                                 Our experienced nurses deliver compassionate, patient-centered care across various specialties. Count on us for flexible, reliable nursing staff to support your healthcare team.
-                                 </p>
-                                 <p class="blog-links">
-                                    By offering flexible solutions, locum GPs support various healthcare settings.
-                                 </p>
-                              </div>
-                              <a class="read-more item-link blog-links">Explore Our Care Services</a>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <!-- Repeat similar blog structure for other columns... -->
-                  <div class="col-xl-6">
-                     <div class="float-left w-100 post-item border mb-4">
-                        <div class="post-item-wrap position-relative">
-                           <div class="post-image">
-                              <div class="CareImage-banner">
-                                 <img alt="" src="assets/image/care-service-banner3.png">
-                              </div>
-                           </div>
-                           <div class="post-item-description blog-component">
-                              <h2 style="font-size: 23px;">
-                                 <a class="blog-links">Healthcare Assistants</a>
-                              </h2>
-                              <p class="blog-links">
-                              Our healthcare staff provides skilled, compassionate care tailored to your needs. We offer flexible staffing solutions to ensure seamless support for your medical practice.
-                              </p>
-                              <!-- Initially hidden content -->
-                              <div class="extra-content hidden">
-                                 <p class="blog-links">
-                                    Locum GPs play a critical role in ensuring that healthcare practices continue
-                                    to provide services even when regular staff are unavailable.
-                                 </p>
-                                 <p class="blog-links">
-                                    By offering flexible solutions, locum GPs support various healthcare settings.
-                                 </p>
-                              </div>
-                              <a class="read-more item-link blog-links">Explore Our Care Services</a>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <!-- Repeat similar blog structure for other columns... -->
-                  <div class="col-xl-6">
-                     <div class="float-left w-100 post-item border mb-4">
-                        <div class="post-item-wrap position-relative">
-                           <div class="post-image">
-                              <div class="CareImage-banner">
-                                 <img alt="" src="assets/image/care-service-banner4.png">
-                              </div>
-                           </div>
-                           <div class="post-item-description blog-component">
-                              <h2 style="font-size: 23px;">
-                                 <a class="blog-links">Pharmacists</a>
-                              </h2>
-                              <p class="blog-links">
-                              Our pharmacists provide expert medication management and personalized care to enhance patient outcomes. We offer flexible staffing solutions to meet your pharmacy service needs
-                              </p>
-                              <!-- Initially hidden content -->
-                              <div class="extra-content hidden">
-                                 <p class="blog-links">
-                                    Locum GPs play a critical role in ensuring that healthcare practices continue
-                                    to provide services even when regular staff are unavailable.
-                                 </p>
-                                 <p class="blog-links">
-                                    By offering flexible solutions, locum GPs support various healthcare settings.
-                                 </p>
-                              </div>
-                              <a class="read-more item-link blog-links">Explore Our Care Services</a>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <!-- Repeat similar blog structure for other columns... -->
-                  <div class="col-xl-6">
-                     <div class="float-left w-100 post-item border mb-4">
-                        <div class="post-item-wrap position-relative">
-                           <div class="post-image">
-                              <div class="CareImage-banner">
-                                 <img alt="" src="assets/image/care-service-banner5.png">
-                              </div>
-                           </div>
-                           <div class="post-item-description blog-component">
-                              <h2 style="font-size: 23px;">
-                                 <a class="blog-links">Rehab</a>
-                              </h2>
-                              <p class="blog-links">
-                              Our rehabilitation specialists focus on personalized recovery plans to help patients regain strength and independence. We provide expert support across physical, occupational, and speech therapies.
-                              </p>
-                              <!-- Initially hidden content -->
-                              <div class="extra-content hidden">
-                                 <p class="blog-links">
-                                    Locum GPs play a critical role in ensuring that healthcare practices continue
-                                    to provide services even when regular staff are unavailable.
-                                 </p>
-                                 <p class="blog-links">
-                                    By offering flexible solutions, locum GPs support various healthcare settings.
-                                 </p>
-                              </div>
-                              <a class="read-more item-link blog-links">Explore Our Care Services</a>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <!-- Repeat similar blog structure for other columns... -->
-                  <div class="col-xl-6">
-                     <div class="float-left w-100 post-item border mb-4">
-                        <div class="post-item-wrap position-relative">
-                           <div class="post-image">
-                              <div class="CareImage-banner">
-                                 <img alt="" src="assets/image/care-service-banner6.png">
-                              </div>
-                           </div>
-                           <div class="post-item-description blog-component">
-                              <h2 style="font-size: 23px;">
-                                 <a class="blog-links">General Medicine</a>
-                              </h2>
-                              <p class="blog-links">
-                              Our general medicine team offers comprehensive care, addressing a wide range of medical conditions with expertise and compassion. We ensure high-quality, personalized treatment for patients of all ages.
-                              </p>
-                              <!-- Initially hidden content -->
-                              <div class="extra-content hidden">
-                                 <p class="blog-links">
-                                    Locum GPs play a critical role in ensuring that healthcare practices continue
-                                    to provide services even when regular staff are unavailable.
-                                 </p>
-                                 <p class="blog-links">
-                                    By offering flexible solutions, locum GPs support various healthcare settings.
-                                 </p>
-                              </div>
-                              <a class="read-more item-link blog-links">Explore Our Care Services</a>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <!-- end coloumns -->
-               </div>
-            </div>
-         </div>
-      </div>
-   </section>
+                    </div>
+                </section>
+                <!-- banner-end -->
    <!-- FOOTER -->
 
 
