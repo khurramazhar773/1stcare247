26fa398ee63dbdb4c1cbc51451db08dc75499606
khurramazhar773 <khurramazhar12346@gmail.com>
2024-10-24 14:54:16 +0500

    Updated

--- care-services.php
+++ care-services.php
@@ -1,4 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="zxx">
 
+<head>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+   <link rel="stylesheet" href="assets/css/animate.css">
+   <link rel="stylesheet" href="assets/bootstarp/bootstrap.min.css">
+   <link rel="stylesheet" href="assets/css/super-classes.css">
+   <link rel="stylesheet" href="assets/css/style.css">
+   <link rel="stylesheet" href="assets/css/custom-style.css">
+   <link rel="stylesheet" href="assets/css/mobile.css">
+
+   <title>Care Services</title>
     <!-- top-bar-section-->
     <?php
     // Attach header.php
