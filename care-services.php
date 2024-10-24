c6963a08217557ba87e9c6b8fda86bbbacdab43f
khurramazhar773 <khurramazhar12346@gmail.com>
2024-10-24 15:02:38 +0500

    Updated

--- care-services.php
+++ care-services.php
@@ -1,53 +1,20 @@
-<!DOCTYPE HTML>
-<html lang="zxx">
 
-<head>
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-   <link rel="stylesheet" href="assets/css/animate.css">
-   <link rel="stylesheet" href="assets/bootstarp/bootstrap.min.css">
-   <link rel="stylesheet" href="assets/css/super-classes.css">
-   <link rel="stylesheet" href="assets/css/style.css">
-   <link rel="stylesheet" href="assets/css/custom-style.css">
-   <link rel="stylesheet" href="assets/css/mobile.css">
-
-   <title>Care Services</title>
-</head>
-
-<body>
-   <!-- top-bar-section-->
-
-   <div class="w-100 float-left top-bar-main-con text-white text-xl-left text-lg-left text-md-left text-center">
-      <div class="container">
-         <div class="row ">
-            <div class="col-lg-6 col-md-6 col-12">
-               <div class="top-bar-left-con ">
-                  <i class="fas fa-map-marker-alt"></i>
-                  <span>King Street Melbourne, 3000, Australia</span>
-               </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-12 d-flex justify-content-xl-end justify-content-lg-end justify-content-md-end justify-content-center">
-               <div class="top-bar-right-con d-flex">
-                  <span>Get Social:</span>
-                  <ul class="list-unstyled mb-0">
-                     <li class="float-left"><a href="https://www.facebook.com/" class="text-white"><i class="fab fa-facebook-square"></i></a></li>
-                     <li class="float-left"><a href="https://twitter.com/?lang=en" class="text-white"><i class="fab fa-twitter-square"></i></a></li>
-                     <li class="float-left"><a href="https://www.pinterest.com/" class="text-white"><i class="fab fa-pinterest-square"></i></a></li>
-                     <li class="float-left"><a href="https://www.youtube.com/" class="text-white"><i class="fab fa-youtube-square"></i></a></li>
-                  </ul>
-               </div>
-            </div>
-         </div>
-      </div>
-   </div>
-   <!-- top-bar-section-->
-   <!-- header-and-banner-section -->
-   <?php require 'navbar.php'; ?>
-
+    <!-- top-bar-section-->
+    <?php
+    // Attach header.php
+    require 'header.php';
+    ?>
+    <!-- top-bar-section-->
     <!-- header-and-banner-section -->
-   <!-- banner-start -->
-   <section>
+    <!-- feature-box -->
+    <div class="w-100 float-left header-and-banner-con banner-overlay-img">
+        <div class="container">
+            <div class="overlay-img">
+                <!-- navbar-start -->
+                <?php require 'nav.php' ?>
+                <!-- navbar-end -->
+                <!-- banner-start -->
+                <section>
                     <div class="w-100 float-left generic-banner-con text-xl-left text-lg-left text-center">
                         <div class="container">
                             <div class="generic-banner-content text-white text-center">
@@ -63,6 +30,209 @@
                     </div>
                 </section>
                 <!-- banner-end -->
+            </div>
+        </div>
+    </div>
+
+
+    <!-- header-and-banner-section -->
+   <section class="blog-posts w-100 float-left mt-5">
+      <div class="container">
+         <div class="row">
+            <div id="blog" class="col-xl-12">
+               <div class="row">
+                  <!-- Example of a blog post -->
+                  <div class="col-xl-6">
+                     <div class="float-left w-100 post-item border mb-4">
+                        <div class="post-item-wrap position-relative">
+                           <div class="post-image">
+                              <div class="CareImage-banner">
+                                 <img alt="" src="assets/image/care-service-banner1.png">
+                              </div>
+                           </div>
+                           <div class="post-item-description blog-component">
+                              <h2 style="font-size: 23px;">
+                                 <a class="blog-links">Locum GP’s</a>
+                              </h2>
+                              <p class="blog-links">
+                              Discover highly skilled locum GPs ready to provide flexible, quality healthcare services when and where you need them. Our experienced team ensures continuity of care tailored to your practice's needs.
+                              </p>
+                              <!-- Initially hidden content -->
+                              <div class="extra-content hidden">
+                                 <p class="blog-links">
+                                    Locum GPs play a critical role in ensuring that healthcare practices continue
+                                    to provide services even when regular staff are unavailable.
+                                 </p>
+                                 <p class="blog-links">
+                                    By offering flexible solutions, locum GPs support various healthcare settings.
+                                 </p>
+                              </div>
+                              <a class="read-more item-link blog-links">Explore Our Care Services</a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!-- Repeat similar blog structure for other columns... -->
+                  <div class="col-xl-6">
+                     <div class="float-left w-100 post-item border mb-4">
+                        <div class="post-item-wrap position-relative">
+                           <div class="post-image">
+                              <div class="CareImage-banner">
+                                 <img alt="" src="assets/image/care-service-banner2.png">
+                              </div>
+                           </div>
+                           <div class="post-item-description blog-component">
+                              <h2 style="font-size: 23px;">
+                                 <a class="blog-links">Nurses</a>
+                              </h2>
+                              <p class="blog-links">
+                                 Our pharmacists provide expert medication management and personalized care to enhance
+                                 patient outcomes. We offer flexible staffing solutions.
+                              </p>
+                              <!-- Initially hidden content -->
+                              <div class="extra-content hidden">
+                                 <p class="blog-links">
+                                 Our experienced nurses deliver compassionate, patient-centered care across various specialties. Count on us for flexible, reliable nursing staff to support your healthcare team.
+                                 </p>
+                                 <p class="blog-links">
+                                    By offering flexible solutions, locum GPs support various healthcare settings.
+                                 </p>
+                              </div>
+                              <a class="read-more item-link blog-links">Explore Our Care Services</a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!-- Repeat similar blog structure for other columns... -->
+                  <div class="col-xl-6">
+                     <div class="float-left w-100 post-item border mb-4">
+                        <div class="post-item-wrap position-relative">
+                           <div class="post-image">
+                              <div class="CareImage-banner">
+                                 <img alt="" src="assets/image/care-service-banner3.png">
+                              </div>
+                           </div>
+                           <div class="post-item-description blog-component">
+                              <h2 style="font-size: 23px;">
+                                 <a class="blog-links">Healthcare Assistants</a>
+                              </h2>
+                              <p class="blog-links">
+                              Our healthcare staff provides skilled, compassionate care tailored to your needs. We offer flexible staffing solutions to ensure seamless support for your medical practice.
+                              </p>
+                              <!-- Initially hidden content -->
+                              <div class="extra-content hidden">
+                                 <p class="blog-links">
+                                    Locum GPs play a critical role in ensuring that healthcare practices continue
+                                    to provide services even when regular staff are unavailable.
+                                 </p>
+                                 <p class="blog-links">
+                                    By offering flexible solutions, locum GPs support various healthcare settings.
+                                 </p>
+                              </div>
+                              <a class="read-more item-link blog-links">Explore Our Care Services</a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!-- Repeat similar blog structure for other columns... -->
+                  <div class="col-xl-6">
+                     <div class="float-left w-100 post-item border mb-4">
+                        <div class="post-item-wrap position-relative">
+                           <div class="post-image">
+                              <div class="CareImage-banner">
+                                 <img alt="" src="assets/image/care-service-banner4.png">
+                              </div>
+                           </div>
+                           <div class="post-item-description blog-component">
+                              <h2 style="font-size: 23px;">
+                                 <a class="blog-links">Pharmacists</a>
+                              </h2>
+                              <p class="blog-links">
+                              Our pharmacists provide expert medication management and personalized care to enhance patient outcomes. We offer flexible staffing solutions to meet your pharmacy service needs
+                              </p>
+                              <!-- Initially hidden content -->
+                              <div class="extra-content hidden">
+                                 <p class="blog-links">
+                                    Locum GPs play a critical role in ensuring that healthcare practices continue
+                                    to provide services even when regular staff are unavailable.
+                                 </p>
+                                 <p class="blog-links">
+                                    By offering flexible solutions, locum GPs support various healthcare settings.
+                                 </p>
+                              </div>
+                              <a class="read-more item-link blog-links">Explore Our Care Services</a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!-- Repeat similar blog structure for other columns... -->
+                  <div class="col-xl-6">
+                     <div class="float-left w-100 post-item border mb-4">
+                        <div class="post-item-wrap position-relative">
+                           <div class="post-image">
+                              <div class="CareImage-banner">
+                                 <img alt="" src="assets/image/care-service-banner5.png">
+                              </div>
+                           </div>
+                           <div class="post-item-description blog-component">
+                              <h2 style="font-size: 23px;">
+                                 <a class="blog-links">Rehab</a>
+                              </h2>
+                              <p class="blog-links">
+                              Our rehabilitation specialists focus on personalized recovery plans to help patients regain strength and independence. We provide expert support across physical, occupational, and speech therapies.
+                              </p>
+                              <!-- Initially hidden content -->
+                              <div class="extra-content hidden">
+                                 <p class="blog-links">
+                                    Locum GPs play a critical role in ensuring that healthcare practices continue
+                                    to provide services even when regular staff are unavailable.
+                                 </p>
+                                 <p class="blog-links">
+                                    By offering flexible solutions, locum GPs support various healthcare settings.
+                                 </p>
+                              </div>
+                              <a class="read-more item-link blog-links">Explore Our Care Services</a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!-- Repeat similar blog structure for other columns... -->
+                  <div class="col-xl-6">
+                     <div class="float-left w-100 post-item border mb-4">
+                        <div class="post-item-wrap position-relative">
+                           <div class="post-image">
+                              <div class="CareImage-banner">
+                                 <img alt="" src="assets/image/care-service-banner6.png">
+                              </div>
+                           </div>
+                           <div class="post-item-description blog-component">
+                              <h2 style="font-size: 23px;">
+                                 <a class="blog-links">General Medicine</a>
+                              </h2>
+                              <p class="blog-links">
+                              Our general medicine team offers comprehensive care, addressing a wide range of medical conditions with expertise and compassion. We ensure high-quality, personalized treatment for patients of all ages.
+                              </p>
+                              <!-- Initially hidden content -->
+                              <div class="extra-content hidden">
+                                 <p class="blog-links">
+                                    Locum GPs play a critical role in ensuring that healthcare practices continue
+                                    to provide services even when regular staff are unavailable.
+                                 </p>
+                                 <p class="blog-links">
+                                    By offering flexible solutions, locum GPs support various healthcare settings.
+                                 </p>
+                              </div>
+                              <a class="read-more item-link blog-links">Explore Our Care Services</a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!-- end coloumns -->
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
    <!-- FOOTER -->
 
 
