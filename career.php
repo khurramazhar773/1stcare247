e96cf42d1946784d3bdd8fc23b033171d25a7fb5
khurramazhar773 <khurramazhar12346@gmail.com>
2024-10-24 14:31:43 +0500

    asd

--- career.php
+++ career.php
@@ -440,39 +440,39 @@
             <div class="row">
                <div class="col-lg-2 col-md-2 col-sm-4 col-4">
                   <figure class="mb-0">
-                     <img src="assets/image/career/collab-1 (1).png" alt="client-logo-img" class="img-fluid">
+                     <img src="assets/image/collab-1 (1).png" alt="client-logo-img" class="img-fluid">
                   </figure>
                </div>
                <div class="col-lg-2 col-md-2 col-sm-4 col-4">
                   <figure class="mb-0">
-                     <img src="assets/image/career/collab-1 (2).png" alt="client-logo-img" class="img-fluid">
+                     <img src="assets/image/collab-1 (2).png" alt="client-logo-img" class="img-fluid">
                   </figure>
                </div>
                <div class="col-lg-2 col-md-2 col-sm-4 col-4">
                   <figure class="mb-0">
-                     <img src="assets/image/career/collab-1 (3).png" alt="client-logo-img" class="img-fluid">
+                     <img src="assets/image/collab-1 (3).png" alt="client-logo-img" class="img-fluid">
                   </figure>
                </div>
                <div class="col-lg-2 col-md-2 col-sm-4 col-4">
                   <figure class="mb-0">
-                     <img src="assets/image/career/collab-1 (4).png" alt="client-logo-img" class="img-fluid mb-0">
+                     <img src="assets/image/collab-1 (4).png" alt="client-logo-img" class="img-fluid mb-0">
                   </figure>
                </div>
                <div class="col-lg-2 col-md-2 col-sm-4 col-4">
                   <figure class="mb-0">
-                     <img src="assets/image/career/collab-1 (5).png" alt="client-logo-img" class="img-fluid mb-0">
+                     <img src="assets/image/collab-1 (5).png" alt="client-logo-img" class="img-fluid mb-0">
                   </figure>
                </div>
                <div class="col-lg-2 col-md-2 col-sm-4 col-4">
                   <figure class="mb-0">
-                     <img src="assets/image/career/collab-1 (4).png" alt="client-logo-img" class="img-fluid mb-0">
+                     <img src="assets/image/collab-1 (3).png" alt="client-logo-img" class="img-fluid mb-0">
                   </figure>
                </div>
             </div>
          </div>
       </div>
    </div>
-   <!-- logo-section -->
+   <!-- logo-section end -->
    <!-- footer-section -->
    <?php
    // Attach footer.php
