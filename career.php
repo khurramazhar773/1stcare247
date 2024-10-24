4981e354b33fa0f6f817595420c93746e6f3d2ae
khurramazhar773 <khurramazhar12346@gmail.com>
2024-10-24 14:49:37 +0500

    Updated

--- career.php
+++ career.php
@@ -161,76 +161,7 @@
    </div>
    <!-- top-bar-section-->
    <!-- header-and-banner-section -->
-   <div class="w-100 float-left header-and-banner-con banner-overlay-img">
-      <div class="container">
-         <div class="overlay-img">
-            <!-- navbar-start -->
-            <nav class="navbar navbar-expand-lg navbar-light">
-               <a class="navbar-brand" href="index.html"><img src="assets/image/logo-img.png" alt="logo-img" class="img-fluid"></a>
-               <button class="navbar-toggler p-0 collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                  <span class="navbar-toggler-icon"></span>
-                  <span class="navbar-toggler-icon"></span>
-               </button>
-               <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul class="navbar-nav ml-auto mr-auto">
-                     <li class="nav-item active ">
-                        <a class="nav-link p-0 text-white" href="index.html">Home<span class="sr-only">(current)</span></a>
-                     </li>
-                     <li class="nav-item">
-                        <a class="nav-link p-0 text-white" href="about.html">About</a>
-                     </li>
-                     <li class="nav-item">
-                        <a class="nav-link p-0 text-white" href="services.html">Services</a>
-                     </li>
-                     <li class="nav-item">
-                        <a class="nav-link p-0 text-white" href="team.html">Team </a>
-                     </li>
-                     <li class="nav-item dropdown pr-lg-0">
-                        <a class="nav-link dropdown-toggle p-0 text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                           Blog
-                        </a>
-                        <div class="dropdown-menu p-0" aria-labelledby="navbarDropdown">
-                           <a class="dropdown-item" href="faq.html">Faq</a>
-                           <a class="dropdown-item" href="four-column.html">four-column</a>
-                           <a class="dropdown-item" href="infinite-scroll.html">infinite-scroll</a>
-                           <a class="dropdown-item" href="load-more.html">load-more</a>
-                           <a class="dropdown-item" href="one-column.html">one-column</a>
-                           <a class="dropdown-item" href="six-colum-full-wide.html">six-colum-full-wide</a>
-                           <a class="dropdown-item" href="three-column.html">three-colum-sidbar</a>
-                           <a class="dropdown-item" href="three-colum-sidbar.html">three-column</a>
-                           <a class="dropdown-item" href="two-column.html">two-column</a>
-                        </div>
-                     </li>
-                     <li class="nav-item pr-0">
-                        <a class="nav-link p-0 text-white" href="contact.html"> Contact </a>
-                     </li>
-                  </ul>
-                  <a href="tel:+12345678" class="navbar-btn text-white">
-                     <i class="fas fa-phone-volume"></i>
-                     +1 234 56 78
-                  </a>
-               </div>
-            </nav>
-            <!-- navbar-end -->
-            <!-- banner-start -->
-            <section>
-               <div class="w-100 float-left generic-banner-con text-xl-left text-lg-left text-center">
-                  <div class="container">
-                     <div class="generic-banner-content text-white text-center">
-                        <h1>Career</h1>
-                        <p class="career-hero-text">Join our team of dedicated healthcare
-                           professionals—explore current opportunities and
-                           take the next step in your career today.</p>
-                     </div>
-
-                  </div>
-               </div>
-            </section>
-            <!-- banner-end -->
-         </div>
-      </div>
-   </div>
+   <?php require 'navbar.php'; ?>
 
    <div class="container">
       <section class="hiring-section">
