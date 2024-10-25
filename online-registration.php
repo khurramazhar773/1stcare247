b313076e7661cce5a0dad59e41aa3330392ac38d
Ali-imran-developer <ai8440018@gmail.com>
2024-10-25 16:24:41 +0500

    today new changes

--- online-registration.php
+++ online-registration.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="assets/image/Group100.svg" type="image/x-icon">
   <title>1stCare | Online Registration</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
